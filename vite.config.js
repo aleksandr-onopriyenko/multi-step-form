3bf1a2f3a419afaf18d901acc8f87426ab063972
aleksandr-onopriyenko <onopriyenkoalex@gmail.com>
2023-05-19 21:14:17 +0300

    fix: update vite config for deploy to github pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://aleksandr-onopriyenko.github.io/multi-step-form/',
   plugins: [react()],
   resolve: {
     alias: {
